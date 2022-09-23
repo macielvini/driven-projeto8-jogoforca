61fdafe6fcefbca98886e247ed2da3af6120c3ce
VInicius Maciel <vm.viniciusmaciel@gmail.com>
2022-09-23 15:58:09 -0300

    Ao clicar na letra certa, o underline é substituido pela letra correta

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,48 @@
 import React from "react";
 import Palavras from "./Palavras";
 
+function print(x) {
+  console.log(x);
+}
+
 export default function App() {
 
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const [letrasClicadas, setLetrasClicadas] = React.useState(alfabeto);
-  let palavra = "";
-  const [resposta, setResposta] = React.useState("Sorteie a primeira palavra");
+
+  const [underlines, setUnderlines] = React.useState("Sorteie a primeira palavra");
   const [erros, setErros] = React.useState(0);
+  const [palavra, setPalavra] = React.useState("");
 
   function atualizarForca() {
     if (erros !== 6) setErros(erros + 1);
   }
 
-  function verificarPalavra(palavra) {
-    setResposta([...palavra].map(() => "_"));
+  function mostrarPalavraNaTela(p) {
+    const palavraArr = [...p];
+    setUnderlines(palavraArr.map(() => "_"));
+  }
+
+  function verificarLetra(letra) {
+    const palavraArr = [...palavra];
+    const newPalavra = palavraArr.map((l, i) => {
+      if (underlines[i] === l) return l;
+      if (letra === l) return l;
+      return "_";
+    })
+
+    print(newPalavra);
+    setUnderlines(newPalavra);
   }
 
   function iniciarJogo() {
+    const novaPalavra = Palavras();
+
     setErros(0);
     setLetrasClicadas([]);
-    verificarPalavra(Palavras());
+    setPalavra(novaPalavra);
+    mostrarPalavraNaTela(novaPalavra);
+    print(novaPalavra);
   }
 
   function Chute() {
@@ -37,11 +59,11 @@ export default function App() {
     return (
       <div className="jogo">
         <div className="forca">
-          <img src={`assets/forca${erros}.png`} alt="forca" onClick={iniciarJogo} />
+          <img src={`assets/forca${erros}.png`} alt="forca" />
         </div>
         <div className="direita">
           <button className="escolher-palavra" onClick={iniciarJogo}>Escolher palavra</button>
-          <p>{resposta}</p>
+          <p>{underlines}</p>
         </div>
       </div >
     )
@@ -64,6 +86,7 @@ export default function App() {
     const { letra, index } = props;
 
     function desativar() {
+      verificarLetra(letra);
       setLetrasClicadas([...letrasClicadas, letra]);
     }
 
